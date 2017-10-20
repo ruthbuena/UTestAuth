2d0864178d22a9a1b7527c45e852774235d45c74
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-19 23:33:20 -0400

    add SignUpForm component

--- client/src/components/BostonCode.js
+++ client/src/components/BostonCode.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Card, CardTitle } from 'material-ui/Card';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Topics from './Topics';
 
 
 const BostonCode = () =>
-  <Router>
-    <div>
-      <Topics />
-    </div>
-  </Router>;
+    <Card className="container">
+      <CardTitle title="React Application" subtitle="This is the home page." />
+      </Card>
+);
+  // <Router>
+  //   <div>
+  //     <Topics />
+  //   </div>
+  // </Router>;
 
 export default BostonCode;
