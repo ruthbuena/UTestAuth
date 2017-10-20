d83b8e860c903e1154425b90a6d2f84b35506927
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-20 18:47:20 -0400

    update routes.js to include Dashboard component and Auth module

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import { browserHistory, Router } from 'react-router';
 import routes from './routes.js';
 
 // remove tap delay, essential for MaterialUI to work properly
@@ -11,12 +12,14 @@ injectTapEventPlugin();
 
 // ReactDom.render(<h1>Hello from React</h1>, document.getElementById('react-app'));
 
-// ReactDom.render((
-//   <MuiThemeProvider muiTheme={getMuiTheme()}>
-//     <Router history={browserHistory} routes={routes} />
-//   </MuiThemeProvider>), document.getElementById('react-app'));
 ReactDom.render((
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-), document.getElementById('react-app'));
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>), document.getElementById('react-app'));
+
+  
+// ReactDom.render((
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+// ), document.getElementById('react-app'));
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,8 @@ import Base from './components/Base.js';
 import HomePage from './components/HomePage.js';
 import LoginPage from './containers/LoginPage.js';
 import SignUpPage from './containers/SignUpPage.js';
-
+import DashboardPage from './containers/DashboardPage.js';
+import Auth from './modules/Auth';
 
 const routes = {
   // base component (wrapper for the whole application).
@@ -11,7 +12,13 @@ const routes = {
 
     {
       path: '/',
-      component: HomePage
+      getComponent: (location, callback) => {
+        if (Auth.isUserAuthenticated()) {
+          callback(null, DashboardPage);
+        } else {
+          callback(null, HomePage);
+        }
+      }
     },
 
     {
@@ -22,6 +29,16 @@ const routes = {
     {
       path: '/signup',
       component: SignUpPage
+    },
+
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        Auth.deauthenticateUser();
+
+        // change the current URL to /
+        replace('/');
+      }
     }
 
   ]
