017960c4a421919cbd98a09f1b48b1ebcdff8036
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-20 18:52:19 -0400

    redirect added to login form after user has successfully signed up

--- client/src/containers/SignUpPage.js
+++ client/src/containers/SignUpPage.js
@@ -7,8 +7,8 @@ class SignUpPage extends React.Component {
   /**
    * Class constructor.
    */
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
 
     // set the initial component state
     this.state = {
@@ -25,10 +25,57 @@ class SignUpPage extends React.Component {
   }
 
   /**
+   * Process the form.
+   *
+   * @param {object} event - the JavaScript event object
+   */
+  processForm(event) {
+    // prevent default action. in this case, action is the form submission event
+    event.preventDefault();
+
+    // create a string for an HTTP body message
+    const name = encodeURIComponent(this.state.user.name);
+    const email = encodeURIComponent(this.state.user.email);
+    const password = encodeURIComponent(this.state.user.password);
+    const formData = `name=${name}&email=${email}&password=${password}`;
+
+    // create an AJAX request
+    const xhr = new XMLHttpRequest();
+    xhr.open('post', '/auth/signup');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', () => {
+      if (xhr.status === 200) {
+        // success
+
+        // change the component-container state
+        this.setState({
+          errors: {}
+        });
+
+        // set a message
+        localStorage.setItem('successMessage', xhr.response.message);
+
+        // make a redirect
+        this.context.router.replace('/login');
+      } else {
+        // failure
+
+        const errors = xhr.response.errors ? xhr.response.errors : {};
+        errors.summary = xhr.response.message;
+
+        this.setState({
+          errors
+        });
+      }
+    });
+    xhr.send(formData);
+  }
+
+  /**
    * Change the user object.
    *
-  //
-   the JavaScript event object
+   * @param {object} event - the JavaScript event object
    */
   changeUser(event) {
     const field = event.target.name;
@@ -41,63 +88,23 @@ class SignUpPage extends React.Component {
   }
 
   /**
-   * Process the form.
-   *
-   * - the JavaScript event object
+   * Render the component.
    */
-  processForm(event) {
-    // prevent default action. in this case, action is the form submission event
-    event.preventDefault();
-
-    // create a string for an HTTP body message
-   const name = encodeURIComponent(this.state.user.name);
-   const email = encodeURIComponent(this.state.user.email);
-   const password = encodeURIComponent(this.state.user.password);
-   const formData = `name=${name}&email=${email}&password=${password}`;
-
-   // create an AJAX request
-   const xhr = new XMLHttpRequest();
-   xhr.open('post', '/auth/signup');
-   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-   xhr.responseType = 'json';
-   xhr.addEventListener('load', () => {
-     if (xhr.status === 200) {
-       // success
-
-       // change the component-container state
-       this.setState({
-         errors: {}
-       });
-
-       console.log('The form is valid');
-     } else {
-       // failure
-
-       const errors = xhr.response.errors ? xhr.response.errors : {};
-       errors.summary = xhr.response.message;
-
-       this.setState({
-         errors
-       });
-     }
-   });
-   xhr.send(formData);
- }
-
- /**
-  * Render the component.
-  */
- render() {
-   return (
-     <SignUpForm
-       onSubmit={this.processForm}
-       onChange={this.changeUser}
-       errors={this.state.errors}
-       user={this.state.user}
-     />
-   );
- }
+  render() {
+    return (
+      <SignUpForm
+        onSubmit={this.processForm}
+        onChange={this.changeUser}
+        errors={this.state.errors}
+        user={this.state.user}
+      />
+    );
+  }
 
 }
+
+SignUpPage.contextTypes = {
+  router: PropTypes.object.isRequired
+};
 
 export default SignUpPage;
